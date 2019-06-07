406dc7e1db31c1f369e7db90c03ed5329d83e077
Rubén Astudillo <ruben.astudillo@ezentis.com>
2019-06-07 10:51:09 +0200

    Se crea enrutador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
+import { APP_ROUTING } from './app.routes';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { CabeceraComponent } from './components/cabecera/cabecera.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CabeceraComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
